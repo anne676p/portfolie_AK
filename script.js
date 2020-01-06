549e8131310bb3843eed5fed5a6b744136fb7de5
Anne-Kathrine Jessien <anne-kathrine@AnneKatrinesMBP.domain_not_set.invalid>
2020-01-06 21:54:14 +0100

    Javascript til dropdown

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("siden vises!");
 }
+
+/*når der klikkes på buttom (knappen), skiftes der mellem hide og show - toggle gør dette*/
+function myFunction() {
+    console.log("Tryk på menu");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
